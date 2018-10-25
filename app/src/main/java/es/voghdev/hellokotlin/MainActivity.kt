e9d18015bb6bc7443c87fb1fa900c2af63978edc
Olmo Gallegos <vendofiatpunto@hotmail.com>
2018-10-25 14:43:52 +0200

    Updated some deps, network is so slow

--- app/src/main/java/es/voghdev/hellokotlin/MainActivity.kt
+++ app/src/main/java/es/voghdev/hellokotlin/MainActivity.kt
@@ -21,9 +21,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.DisplayMetrics
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
-import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,16 +31,6 @@ class MainActivity : AppCompatActivity() {
 
         button1.setOnClickListener {
             toast("You wrote: ${editText1.text}")
-
-            doAsync() {
-                Thread.sleep(2500)
-                uiThread {
-                    val width: Int = screenWidth()
-                    val height: Int = screenHeight()
-
-                    toast("Screen size is: $width x $height")
-                }
-            }
         }
     }
 
--- app/src/test/java/es/voghdev/hellokotlin/SomeDetailPresenterTest.kt
+++ app/src/test/java/es/voghdev/hellokotlin/SomeDetailPresenterTest.kt
@@ -4,12 +4,6 @@ package es.voghdev.hellokotlin
 import com.nhaarman.mockito_kotlin.argumentCaptor
 import com.nhaarman.mockito_kotlin.whenever
 import es.voghdev.hellokotlin.domain.ResLocator
-import es.voghdev.hellokotlin.features.invoice.Invoice
-import es.voghdev.hellokotlin.features.user.SomeDetailPresenter
-import es.voghdev.hellokotlin.features.user.User
-import es.voghdev.hellokotlin.features.user.UserRepository
-import es.voghdev.hellokotlin.features.user.usecase.GetUsers
-import es.voghdev.hellokotlin.features.user.usecase.InsertUser
 import junit.framework.Assert.assertNotNull
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Assert.assertEquals
@@ -22,14 +16,14 @@ import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
-class SomeDetailPresenterTest {
+class MainPresenterTest {
     @Mock
     lateinit var mockUserRepository: UserRepository
 
     @Mock lateinit var mockResLocator: ResLocator
 
     @Mock
-    lateinit var mockView: SomeDetailPresenter.MVPView
+    lateinit var mockView: MainPresenter.MVPView
 
     @Mock
     lateinit var mockGetUsersApi: GetUsers
@@ -40,116 +34,19 @@ class SomeDetailPresenterTest {
     @Mock
     lateinit var mockInsertUser: InsertUser
 
-    lateinit var presenter: SomeDetailPresenter
+    lateinit var presenter: MainPresenter
 
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
 
-        presenter = SomeDetailPresenter(mockResLocator, mockUserRepository)
+        presenter = MainPresenter(mockResLocator, mockUserRepository)
         presenter.view = mockView
     }
 
     @Test
-    fun `should request a List of users on start`() {
-        givenAllStringsAreMocked()
+    fun `should add two values`() {
 
-        assertNotNull(presenter)
-
-        presenter.initialize().await()
-
-        verify(mockUserRepository, times(1))?.getUsers()
-    }
-
-    @Test
-    fun `should show user list if request has results`() {
-        givenAllStringsAreMocked()
-
-        whenever(mockUserRepository.getUsers()).thenReturn(listOf(User(name = "John")))
-
-        assertNotNull(presenter)
-
-        presenter.initialize().await()
-
-        verify(mockView, times(1))?.showUsers(anyList())
-    }
-
-    @Test
-    fun `should show empty case if request has no results`() {
-        givenAllStringsAreMocked()
-
-        assertNotNull(presenter)
-
-        presenter.initialize().await()
-
-        verify(mockView, times(1))?.showEmptyCase()
-    }
-
-    @Test
-    fun `should call a simple method containing a coroutine in runBlocking mode`() {
-        runBlocking {
-            presenter.onSomeEventHappened()
-        }
-
-        verify(mockView).showSomeResult()
-    }
-
-    @Test
-    fun `should call a method that contains a coroutine and returns a result using runBlocking`() {
-        whenever(mockUserRepository.performSomeBlockingOperationWithResult()).thenReturn(listOf(
-                User(name = "User 001"),
-                User(name = "User 002")
-        ))
-
-        runBlocking {
-            presenter.onSomeOtherEventHappened()
-        }
-
-        val captor = argumentCaptor<List<User>>()
-
-        verify(mockView).showUsers(captor.capture())
-
-        assertEquals(2, captor.firstValue.size)
-        assertEquals("User 001", captor.firstValue[0].name)
-        assertEquals("User 002", captor.firstValue[1].name)
-    }
-
-    @Test
-    fun `should call a method that contains a coroutine and verify assertions in the repository`() {
-        val invoice = Invoice(customerId = 15L, amount = 10f)
-
-        runBlocking {
-            presenter.onEventWithParameterHappened(invoice.customerId)
-        }
-
-        val captor = argumentCaptor<Invoice>()
-
-        verify(mockUserRepository).performSomeBlockingOperationWithAParameter(captor.capture())
-        verify(mockView).showSomeResult()
-
-        assertEquals(15L, captor.firstValue.customerId)
-        assertEquals(50f, captor.firstValue.amount)
-        assertNotEquals(10f, captor.firstValue.amount)
-    }
-
-    @Test
-    fun `should call a method that contains a coroutine using a non-mocked repository with mock DataSources`() {
-        val nonMockedRepository = UserRepository(mockGetUsersApi, mockGetUsersDb, mockInsertUser)
-
-        val presenter = SomeDetailPresenter(mockResLocator, nonMockedRepository)
-        presenter.view = mockView
-
-        runBlocking {
-            presenter.onSomeOtherEventHappened()
-        }
-
-        val captor = argumentCaptor<List<User>>()
-
-        verify(mockView).showUsers(captor.capture())
-
-        assertEquals(2, captor.firstValue.size)
-        assertEquals("John doe", captor.firstValue[0].name)
-        assertEquals("Jane doe", captor.firstValue[1].name)
     }
 
     private fun givenAllStringsAreMocked() {
